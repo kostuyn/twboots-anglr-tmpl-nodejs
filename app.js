929875ad945faab285fd7932790a43b74650e4ba
altuhov <altuhov@clickberry.com>
2015-08-06 13:33:54 +0300

    update

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ mongoose.connect(config.get('mongo_connection'));
 require('./config/passport')(passport);
 var app = express();
 
-console.log(config.get('my_env'));
-console.log(config.get('MY_env'));
-console.log(config.get('token_secret'));
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,7 +24,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
